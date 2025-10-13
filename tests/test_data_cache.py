5e04dd962974d349210f07be8f0f63b713d7aa3d
hruedisser <hannah@ruedisser.at>
2025-10-13 10:38:59 +0200

    add test folder for test files

---
+++ tests/test_data_cache.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+from src.data.data_cache import get_data_cache
+
+idds = {
+    "STEREO_A": "ICME_STEREO_A_MOESTL_20240324_01", # this is the ID of the event we want to analyze
+    "SOLO": "ICME_SOLO_MOESTL_20240323_01",
+    "BEPI": 'ICME_BEPI_MOESTL_20240324_01'
+}
+
+for idd_key, idd_val in idds.items():
+    
+    try:
+        print(f"Trying example for {idd_key}")
+
+        data_cache = get_data_cache(idd_val, mean_hours=24) # mean_hours is the number of hours before the event to calculate the mean solar wind speed
+
+        print(f"Success for {idd_key}")
+    
+    except Exception as e:
+        print(f"Failed for {idd_key} with error: {e}")
+        continue
+
+    
